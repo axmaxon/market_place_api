642442088e2c8bf357737b140f2d61e8a6bdd986
maxim <axmaxon@gmail.com>
2022-02-20 15:43:21 +0500

    setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
+
+  # Шифрует пароли путем хеширования - генерирует 'password_digest'
+  # из атрибута 'password', указанного при создании юзера
+  has_secure_password
 end
