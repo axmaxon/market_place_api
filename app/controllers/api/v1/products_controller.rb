d5fdf2010fee061021da13a2b79e5e33d604f4fc
maxim <axmaxon@gmail.com>
2022-02-25 17:18:20 +0500

    add comments

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ProductsController < ApplicationController
+  # Вернёт код 403 (forbidden) в заголовке ответа если current_user - falsy
   before_action :check_login, only: %i[create]
 
+  # Создаёт новый инстанс продукта [в ассоциации] с юзером
   def create
     product = current_user.products.build(product_params)
 
