7d88215804fd879da8af400a164eadb972fdaa03
maxim <axmaxon@gmail.com>
2022-03-01 11:51:30 +0500

    add a scope for sorting by "updated_at" field to the Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,8 @@ class Product < ApplicationRecord
 
   belongs_to :user
 
-  scope :filter_by_title, lambda { |keyword| where('lower(title) LIKE ?', "%#{keyword.downcase}%")}
+  scope :filter_by_title, lambda { |keyword| where('lower(title) LIKE ?', "%#{keyword.downcase}%") }
   scope :above_or_equal_to_price, lambda { |price| where('price >= ?', price) }
   scope :below_or_equal_to_price, lambda { |price| where('price <= ?', price) }
+  scope :recent, lambda { order(:updated_at) }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -22,4 +22,10 @@ class ProductTest < ActiveSupport::TestCase
   test "should filter products by price lower and sort them" do
     assert_equal [products(:another_tv)], Product.below_or_equal_to_price(200).sort
   end
+
+  test "should sort product by most recent" do
+    # touch (без аргументов) обновит поле продукта update_at текущим временем
+    products(:two).touch
+    assert_equal [products(:one), products(:another_tv), products(:two)], Product.recent.to_a
+  end
 end
