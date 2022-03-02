a2049d5629e9aa20aca2f1323945bda1e1af457a
maxim <axmaxon@gmail.com>
2022-03-02 10:42:40 +0500

    fix a couple of codestyle mistakes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
+  has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
-
-  has_many :products, dependent: :destroy
-  has_many :orders, dependent: :destroy
 
   # Шифрует пароли путем хеширования - генерирует 'password_digest'
   # из атрибута 'password', указанного при создании юзера
