e6494970e2882b8c5989fea022f3cb1d806c95fe
maxim <axmaxon@gmail.com>
2022-03-03 14:37:47 +0500

    add Orders#show

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -4,4 +4,17 @@ class Api::V1::OrdersController < ApplicationController
   def index
     render json: OrderSerializer.new(current_user.orders).serializable_hash
   end
+
+  def show
+    order = current_user.orders.find(params[:id])
+
+    if order
+      # Опции для включения в вывод атрибутов продуктов, которые связаны с заказом
+      # (требуется также указание связи в сериализаторе)
+      options = { include: [:products] }
+      render json: OrderSerializer.new(order, options).serializable_hash
+    else
+      head 404
+    end
+  end
 end
