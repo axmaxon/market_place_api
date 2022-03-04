3d683f5a005c65f6b91c0bb6480db57cff303975
maxim <axmaxon@gmail.com>
2022-03-04 14:44:52 +0500

    implement the order total attribute setting through the  model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :user
+
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
 
+  before_validation :set_total!
+
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
+
+  # Стоимость заказа формируется динамически, по товарам содержащимся в заказе
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
